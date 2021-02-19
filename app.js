61a7f750c69b79d5a0887120ac9e434b4055ecf4
David Bin Neun <davidbinneun@gmail.com>
2021-02-19 13:41:21 +0200

    removed unneeded files

--- app.js
+++
@@ -1,7 +0,0 @@
-const EventEmitter = require('events');
-const emitter = new EventEmitter();
-
-const files = fs.readdir('./', function(err, files){
-  if (err) console.log('Error', err);
-  else console.log('Result', files)
-}); 
